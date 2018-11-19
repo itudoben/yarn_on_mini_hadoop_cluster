848c6fe1fd0d9586323b2b9728de132d2994b5ec
Johnny <johnny.hujol@gmail.com>
2018-11-19 19:27:36 +0100

    ADD example from Spark website

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Introduction
 This project allows to run a Java YARN application on a macOS pc hosting a mini hadoop cluster.
+
+# Running the app in Spark
+
+Based on http://spark.apache.org/docs/latest/quick-start.html
+
+```bash
+mvn package
+
+spark-submit --class "com.jh.yarnminihadoop.App"   --master local[4]   target/yarnmain-0.1.0.jar
+```
--- src/main/java/com/jh/yarnminihadoop/App.java
+++ src/main/java/com/jh/yarnminihadoop/App.java
@@ -1,14 +1,34 @@
 package com.jh.yarnminihadoop;
 
-import org.apache.hadoop.yarn.client.api.YarnClient;
+//import org.apache.hadoop.yarn.client.api.YarnClient;
+
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
 
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
-        YarnClient yarnClient = YarnClient.createYarnClient();
-        yarnClient.init(conf);
-        yarnClient.start();
+//        YarnClient yarnClient = YarnClient.createYarnClient();
+//        yarnClient.init(conf);
+//        yarnClient.start();
+
+
+        String logFile = "/Users/hujol/Projects/distributed_computing/yarn_on_mini_hadoop_cluster/README.md"; // Should be some file on your system
+        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        FilterFunction<String> filterFunction = s -> s.contains("a");
+        long numAs = logData.filter(filterFunction).count();
+        FilterFunction<String> fff = s -> s.contains("b");
+        long numBs = logData.filter(fff).count();
+
+        System.out.println("*** ===> Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
     }
 }
